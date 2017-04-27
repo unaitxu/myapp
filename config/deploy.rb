47a86115b3f4d8b7fe9bdb2a3b89ff12c3884973
Unai Martinez <unaimartinezrodriguez@gmail.com>
2017-04-27 18:02:43 +0200

    Change url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.1"
 
 set :application, "myapp"
-set :repo_url, "git@example.com:unaitxu/my_app.git"
+set :repo_url, "git@github.com:unaitxu/my_app.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
