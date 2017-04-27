4b1ca1d5984ba8dac4a1384378a18a73a2d585b6
Unai Martinez <unaimartinezrodriguez@gmail.com>
2017-04-27 18:49:17 +0200

    changes in puma

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ group :development do
   gem "capistrano", "~> 3.8"
   gem 'capistrano-rails'
   gem 'capistrano-rbenv'
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano3-puma',   require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/deploy.rb
+++ config/deploy.rb
@@ -36,3 +36,12 @@ server 'ec2-52-29-213-82.eu-central-1.compute.amazonaws.com', user: 'deploy', ro
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_control_app, false
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
