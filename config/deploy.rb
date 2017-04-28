b1f4b462552e2650b1362db9bb59b5dd56e8dc3c
Unai Martinez <unaimartinezrodriguez@gmail.com>
2017-04-28 16:44:41 +0200

    Clean up

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,38 +4,13 @@ lock "3.8.1"
 set :application, "my_app"
 set :repo_url, "git@github.com:unaitxu/myapp.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :deploy_to, '/home/deploy/my_app'
 
 append :linked_files, "config/database.yml", "config/secrets.yml", ".rbenv-vars"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-server 'ec2-52-29-213-82.eu-central-1.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
+server 'ec2-52-28-158-69.eu-central-1.compute.amazonaws.com', user: 'deploy', roles: %w{app db web}
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
